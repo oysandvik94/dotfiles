e26a78f7e64b68092b85cdb59e6e106de333d210
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2024-02-19 18:32:41 +0100

    feat(nvim): http fil per prosjekt

--- .config/nvim/lua/langeoys/plugins/rest.lua
+++ .config/nvim/lua/langeoys/plugins/rest.lua
@@ -8,7 +8,6 @@ return {
 		})
 
 		vim.api.nvim_create_user_command("Http", function()
-			-- open new tab
 			vim.cmd.tabnew()
 			local session_http = vim.fn.stdpath("data") .. "/session_http"
 			vim.cmd.edit(session_http)
@@ -16,11 +15,19 @@ return {
 			vim.o.buftype = ""
 		end, { desc = "Send HTTP request" })
 
+		vim.api.nvim_create_user_command("HttpProject", function()
+			vim.cmd.tabnew()
+			local cwd_slug = vim.fn.getcwd():match("([^/]+)$")
+			local session_http = vim.fn.stdpath("data") .. "/session_http_" .. cwd_slug
+			vim.cmd.edit(session_http)
+			vim.o.filetype = "http"
+			vim.o.buftype = ""
+		end, { desc = "HTTP workspace for this project" })
+
 		vim.api.nvim_create_augroup("RestNvim", {})
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "http",
 			callback = function()
-				vim.o.wrap = false
 
 				vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<cmd>lua require('rest-nvim').run()<CR>", {})
 				vim.api.nvim_buf_set_keymap(0, "n", "<leader>lr", "<cmd>lua require('rest-nvim').last()<CR>", {})
