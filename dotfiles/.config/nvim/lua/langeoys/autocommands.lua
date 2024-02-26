bdb16eed7a26884c224b7980af84093dbc6b9eaa
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2024-02-26 13:55:15 +0100

    feat(nvim): c filetype for zmk/qmk keymap files

--- dotfiles/.config/nvim/lua/langeoys/autocommands.lua
+++ dotfiles/.config/nvim/lua/langeoys/autocommands.lua
@@ -1,8 +1,10 @@
 vim.api.nvim_create_autocmd("BufReadPost", {
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
-            vim.api.nvim_win_set_cursor(0, mark)
-        end
-    end,
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+			vim.api.nvim_win_set_cursor(0, mark)
+		end
+	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = "*.keymap", command = "set filetype=c" })
