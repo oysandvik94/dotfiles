7260b1e7f731fd44ef086a6f92d0ad0b838c20c6
oysandvik94 <oysandvik94@gmail.com>
2024-03-15 21:28:16 +0100

    Neogit

---
+++ dotfiles/.config/nvim/lua/langeoys/plugins/git.lua
@@ -0,0 +1,17 @@
+return {
+        "NeogitOrg/neogit",
+        dependencies = {
+                "nvim-lua/plenary.nvim",  -- required
+                "sindrets/diffview.nvim", -- optional - Diff integration
+
+                -- Only one of these is needed, not both.
+                "nvim-telescope/telescope.nvim", -- optional
+                "ibhagwan/fzf-lua",              -- optional
+        },
+        config = function()
+                -- init.lua
+                local neogit = require('neogit')
+                neogit.setup {}
+                vim.keymap.set("n", "<leader>g", function() neogit.open() end)
+        end
+}
