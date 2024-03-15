dff14b463fda889679dd6b962d069741d9f68acf
oysandvik94 <oysandvik94@gmail.com>
2024-03-15 21:26:33 +0100

    Upgrade to harpoon 2.0

--- dotfiles/.config/nvim/lua/langeoys/plugins/harpoon.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/harpoon.lua
@@ -1,25 +1,21 @@
 return {
-    'ThePrimeagen/harpoon',
-    config = function()
-        require("harpoon").setup({
-            tabline = false,
-        })
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+                local harpoon = require("harpoon")
+                harpoon:setup()
 
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+                vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+                vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add file to harpoon list" })
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle harpoon window" })
+                vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+                vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+                vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+                vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
 
-        vim.keymap.set("n", "<A-1>", function() ui.nav_file(1) end, { desc = "Go to first harpooned file" })
-        vim.keymap.set("n", "<A-2>", function() ui.nav_file(2) end, { desc = "Go to second harpooned file" })
-        vim.keymap.set("n", "<A-3>", function() ui.nav_file(3) end, { desc = "Go to third harpooned file" })
-        vim.keymap.set("n", "<A-4>", function() ui.nav_file(4) end, { desc = "Go to fourth harpooned file" })
-
-        vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#6E6A86')
-        vim.cmd('highlight! HarpoonActive guibg=NONE guifg=#F6C177')
-        vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#EA9A97')
-        vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#6E6A86')
-        vim.cmd('highlight! TabLineFill guibg=NONE guifg=#F6C177')
-    end
+                -- Toggle previous & next buffers stored within Harpoon list
+                vim.keymap.set("n", "]h", function() harpoon:list():prev() end)
+                vim.keymap.set("n", "[h", function() harpoon:list():next() end)
+        end
 }
