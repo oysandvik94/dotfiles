88b7e2eb52f8e78eb39854453ec1b5257e843878
oysandvik94 <oysandvik94@gmail.com>
2024-04-24 11:26:00 +0200

    Inherit terminal background in neovim

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -4,6 +4,12 @@ require("langeoys")
 local colorscheme = require("langeoys.utils.state").get_state("colorscheme") or "rose-pine"
 if colorscheme then
 	local _, err = pcall(function() vim.cmd("colorscheme " .. colorscheme) end)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNCFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+	vim.api.nvim_set_hl(0, "FzfLuaBorder", { bg = "none" })
 
 	if err then
 		P("Failed to load colorscheme: " .. colorscheme)
