48b2c086bbe321e33445737701c7eb855f8b7f59
oysandvik94 <oysandvik94@gmail.com>
2024-04-24 11:44:02 +0200

    Bruk alt i harpoon og replace

--- dotfiles/.config/nvim/lua/langeoys/plugins/harpoon.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/harpoon.lua
@@ -6,13 +6,18 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "A-a", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "A-r", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "A-s", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "A-s", function() harpoon:list():select(4) end)
+
+        vim.keymap.set("n", "<leader>A-a", function() harpoon:list():replace_at(1) end)
+        vim.keymap.set("n", "<leader>A-r", function() harpoon:list():replace_at(2) end)
+        vim.keymap.set("n", "<leader>A-s", function() harpoon:list():replace_at(3) end)
+        vim.keymap.set("n", "<leader>A-s", function() harpoon:list():replace_at(4) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "]h", function() harpoon:list():prev() end)
