f3004384def76cba192d8ea9181d4a3c520d26fb
Øystein Sandvik <oysandvik94@gmail.com>
2024-05-01 22:45:21 +0200

    Legg til util function for å gå til rust error nettside

---
+++ dotfiles/.config/nvim/lua/langeoys/utils/rust.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.go_to_error = function()
+	local diagnostics = vim.diagnostic.get()
+
+	for _, diagnostic in ipairs(diagnostics) do
+		local user_data = diagnostic.user_data
+		-- P(user_data)
+		if user_data.lsp and user_data.lsp.codeDescription and user_data.lsp.codeDescription.href then
+			local url = user_data.lsp.codeDescription.href
+			vim.ui.open(url)
+		end
+	end
+end
+
+return M
