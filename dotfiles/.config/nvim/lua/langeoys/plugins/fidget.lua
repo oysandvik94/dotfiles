b0349fe667513cd05a2b3e4d09b967dd51c5e5ee
Øystein Sandvik <oysandvik94@gmail.com>
2024-05-01 22:45:15 +0200

    Fix fidget

--- dotfiles/.config/nvim/lua/langeoys/plugins/fidget.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/fidget.lua
@@ -1,9 +1,8 @@
 return {
 	"j-hui/fidget.nvim",
-    enabled = false,
 	config = function()
-		require("fidget").setup({
-			-- options
-		})
+		require("fidget").setup {
+			notification = { window = { winblend = 0 } }
+		}
 	end,
 }
