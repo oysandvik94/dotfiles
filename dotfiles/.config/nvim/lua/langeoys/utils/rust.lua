6a4067c494037259fde389731ac7d559fa1600b3
oysandvik94 <oysandvik94@gmail.com>
2024-05-03 09:20:39 +0200

    Bare åpne diagnostic på nåværende linjenummer

--- dotfiles/.config/nvim/lua/langeoys/utils/rust.lua
+++ dotfiles/.config/nvim/lua/langeoys/utils/rust.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 M.go_to_error = function()
-	local diagnostics = vim.diagnostic.get()
+	local current_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+	local diagnostics = vim.diagnostic.get(0, { lnum = (current_line - 1) })
 
 	for _, diagnostic in ipairs(diagnostics) do
 		local user_data = diagnostic.user_data
-		-- P(user_data)
 		if user_data.lsp and user_data.lsp.codeDescription and user_data.lsp.codeDescription.href then
 			local url = user_data.lsp.codeDescription.href
 			vim.ui.open(url)
