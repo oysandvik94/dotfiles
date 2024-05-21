f3dfd0df815072f95bc31e4e312c4edd7ddba726
oysandvik94 <oysandvik94@gmail.com>
2024-05-21 22:04:40 +0200

    Fjern markdown linting

--- dotfiles/.config/nvim/lua/langeoys/plugins/linter.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/linter.lua
@@ -2,7 +2,6 @@ return {
 	"mfussenegger/nvim-lint",
 	config = function()
 		require("lint").linters_by_ft = {
-			markdown = { "markdownlint" },
 			-- Bruker eslint lsp isteden
 			-- typescript = { "eslint_d" },
 			-- javascript = { "eslint_d" },
