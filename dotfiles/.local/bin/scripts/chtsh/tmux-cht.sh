927c9fcc615ebce1392077ef90c830f7b20eb5c8
oysandvik94 <oysandvik94@gmail.com>
2024-05-24 12:04:11 +0200

    Use man page in cheatsheet

--- dotfiles/.local/bin/scripts/chtsh/tmux-cht.sh
+++ dotfiles/.local/bin/scripts/chtsh/tmux-cht.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
-selected=`cat ~/.local/bin/scripts/chtsh/.tmux-cht-languages ~/.local/bin/scripts/chtsh/.tmux-cht-custom ~/.local/bin/scripts/chtsh/.tmux-cht-command | fzf`
+selected=$(cat ~/.local/bin/scripts/chtsh/.tmux-cht-languages ~/.local/bin/scripts/chtsh/.tmux-cht-custom ~/.local/bin/scripts/chtsh/.tmux-cht-command | fzf)
 if [[ -z $selected ]]; then
-    exit 0
+	exit 0
 fi
 
 if [[ $selected == 'wiki' ]]; then
-    tmux neww bash -c "zk edit --interactive"
-    exit 0
+	tmux neww bash -c "zk edit --interactive"
+	exit 0
 fi
 
-read -p "Enter Query: " query
+read -rp "Enter Query: " query
+
+if [[ $selected == 'man' ]]; then
+	tmux neww bash -c "man $query"
+	exit 0
+fi
 
 if grep -qs "$selected" ~/.local/bin/scripts/chtsh/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "curl -s https://cht.sh/$selected/$query | less -R"
+	query=$(echo $query | tr ' ' '+')
+	tmux neww bash -c "curl -s https://cht.sh/$selected/$query | less -R"
 else
-    tmux neww bash -c "curl -s https://cht.sh/$selected~$query | less -R"
+	tmux neww bash -c "curl -s https://cht.sh/$selected~$query | less -R"
 fi
