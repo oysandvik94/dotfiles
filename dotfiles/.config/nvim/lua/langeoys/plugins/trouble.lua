0c9ac5315efa607fb6d4a8635dce88f7f1fbe8b6
oysandvik94 <oysandvik94@gmail.com>
2024-06-03 10:38:26 +0200

    nvim: use new trouble version

--- dotfiles/.config/nvim/lua/langeoys/plugins/trouble.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/trouble.lua
@@ -1,27 +1,30 @@
 return {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        auto_open = true,
-        auto_preview = false,
-        position = "left"
-    },
-    config = function()
-        require("trouble").setup({
-            auto_preview = false,
-        })
-        -- Lua
-        vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-        vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-        vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-        vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-        vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-        vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
-        vim.keymap.set("n", "]x", function()
-            require("trouble").next({ skip_groups = true, jump = true });
-        end)
-        vim.keymap.set("n", "[x", function()
-            require("trouble").previous({ skip_groups = true, jump = true });
-        end)
-    end
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		auto_open = true,
+		auto_preview = false,
+		position = "left",
+	},
+	config = function()
+		require("trouble").setup({
+			auto_preview = false,
+		})
+		-- Lua
+		vim.keymap.set("n", "<leader>xx", "<cmd>Trouble quickfix toggle<CR>")
+		vim.keymap.set("n", "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>")
+		vim.keymap.set("n", "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>")
+		vim.keymap.set("n", "<leader>xq", function()
+			require("trouble").toggle("quickfix")
+		end)
+		vim.keymap.set("n", "gR", "<cmd>Trouble lsp_references toggle<CR>")
+		vim.keymap.set("n", "gR", "<cmd>Trouble lsp_references toggle<CR>")
+		vim.keymap.set("n", "gs", "<cmd>Trouble symbols toggle<CR>")
+		vim.keymap.set("n", "]x", function()
+			require("trouble").next({ skip_groups = true, jump = true })
+		end)
+		vim.keymap.set("n", "[x", function()
+			require("trouble").previous({ skip_groups = true, jump = true })
+		end)
+	end,
 }
