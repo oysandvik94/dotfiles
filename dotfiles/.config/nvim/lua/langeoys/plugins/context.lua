832d1168b77569d1c86e6de852f66e94196bed09
oysandvik94 <oysandvik94@gmail.com>
2024-06-03 10:36:08 +0200

    nvim: add maxlines to ts context

--- dotfiles/.config/nvim/lua/langeoys/plugins/context.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/context.lua
@@ -1,10 +1,12 @@
 return {
-    "nvim-treesitter/nvim-treesitter-context",
-    config = function()
-        require("nvim-treesitter.configs").setup {
-            context = {
-                enable = true,
-            }
-        }
-    end
+	"nvim-treesitter/nvim-treesitter-context",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			context = {
+				enable = true,
+				max_lines = 3,
+				multiline_threshold = 3,
+			},
+		})
+	end,
 }
