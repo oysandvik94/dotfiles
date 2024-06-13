ced0f8530a6c5205b19892ae6cc6f214db6c05bd
oysandvik94 <oysandvik94@gmail.com>
2024-06-13 15:04:09 +0200

    nvim: fix autocommand til git commit slik at den fungerer utenom neogit

--- dotfiles/.config/nvim/lua/langeoys/autocommands.lua
+++ dotfiles/.config/nvim/lua/langeoys/autocommands.lua
@@ -1,31 +1,28 @@
-
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = "*.keymap", command = "set filetype=c" })
 
 local function write_commit_prefix()
-    -- Run the Git command to get the current branch name
-    local handle = io.popen("git branch --show-current")
-    local branch = handle:read("*a")
-    handle:close()
+	-- Run the Git command to get the current branch name
+	local handle = io.popen("git branch --show-current")
+	local branch = handle:read("*a")
+	handle:close()
 
-    -- Trim whitespace from the branch name
-    branch = string.gsub(branch, "^%s*(.-)%s*$", "%1")
+	-- Trim whitespace from the branch name
+	branch = string.gsub(branch, "^%s*(.-)%s*$", "%1")
 
-    -- Check if the branch name contains 'UT-XXXX'
-    local ut_number = string.match(branch, "UT%-%d%d%d%d")
-    if ut_number then
-        -- Insert 'UT-XXXX: ' at the beginning of the buffer
-        local insert_text = ut_number .. ": "
-        vim.api.nvim_buf_set_lines(0, 0, 0, false, {insert_text})
-
+	-- Check if the branch name contains 'UT-XXXX'
+	local ut_number = string.match(branch, "UT%-%d%d%d%d")
+	if ut_number then
+		-- Insert 'UT-XXXX: ' at the beginning of the buffer
+		local insert_text = ut_number .. ": "
+		vim.api.nvim_buf_set_lines(0, 0, 1, false, { insert_text })
 
 		-- put the cursor in insert mode at the end of the line
-		vim.api.nvim_feedkeys("A", "n", true);
-    end
+	end
+	vim.api.nvim_feedkeys("A", "n", true)
 end
 
 -- Set up the autocommand for NeogitCommitMessage filetype
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "NeogitCommitMessage",
-    callback = write_commit_prefix
+	pattern = "gitcommit",
+	callback = write_commit_prefix,
 })
-
