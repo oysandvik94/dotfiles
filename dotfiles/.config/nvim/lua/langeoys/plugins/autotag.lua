7663eb29806d5ac5c66103c5c0f87dc51ae53aae
Øystein Sandvik <oysandvik94@gmail.com>
2024-07-03 13:54:22 +0200

    nvim: enable autotag

--- dotfiles/.config/nvim/lua/langeoys/plugins/autotag.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/autotag.lua
@@ -1,10 +1,6 @@
 return {
-    "windwp/nvim-ts-autotag",
-    config = function()
-        require 'nvim-treesitter.configs'.setup {
-            autotag = {
-                enable = true,
-            }
-        }
-    end
+	"windwp/nvim-ts-autotag",
+	config = function()
+		require("nvim-ts-autotag").setup({})
+	end,
 }
