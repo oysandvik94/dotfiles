7c8ee5df313ea8fd6142116870583f927bc80590
Øystein Sandvik <oysandvik94@gmail.com>
2024-07-03 14:03:38 +0200

    nvim: change surround plugin

--- dotfiles/.config/nvim/lua/langeoys/plugins/surround.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/surround.lua
@@ -1,3 +1,9 @@
 return {
-    "machakann/vim-sandwich"
+	"kylechui/nvim-surround",
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({
+			-- Configuration here, or leave empty to use defaults
+		})
+	end,
 }
