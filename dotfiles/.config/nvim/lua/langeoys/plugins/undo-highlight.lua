ba3a0dad3968b608a441878dbc5679eef2499544
Øystein Sandvik <oysandvik94@gmail.com>
2024-07-03 14:04:18 +0200

    nvim: format file

--- dotfiles/.config/nvim/lua/langeoys/plugins/undo-highlight.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/undo-highlight.lua
@@ -1,6 +1,6 @@
 return {
-    'tzachar/highlight-undo.nvim',
-    config = function()
-        require('highlight-undo').setup({ })
-    end
+	"tzachar/highlight-undo.nvim",
+	config = function()
+		require("highlight-undo").setup({})
+	end,
 }
