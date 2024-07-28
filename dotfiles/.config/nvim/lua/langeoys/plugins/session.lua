bf88854aa884cc998ca5c643b7228b01228dcb0c
Øystein Sandvik <oysandvik94@gmail.com>
2024-07-28 12:27:27 +0200

    nvim: use nvim_session_manager again instead

---
+++ dotfiles/.config/nvim/lua/langeoys/plugins/session.lua
@@ -0,0 +1,33 @@
+return {
+	"Shatur/neovim-session-manager",
+	config = function()
+		local Path = require("plenary.path")
+		local config = require("session_manager.config")
+
+		require("session_manager").setup({
+			sessions_dir = Path:new(vim.fn.stdpath("data"), "sessions"), -- The directory where the session files will be saved.
+			autoload_mode = config.AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+			autosave_ignore_buftypes = {}, -- All buffers of these bufer types will be closed before the session is saved.
+			autosave_ignore_filetypes = { "curl" },
+		})
+
+		vim.keymap.set(
+			"n",
+			"<leader>wr",
+			"<cmd>SessionManager load_last_session<CR>",
+			{ desc = "Restore session for cwd" }
+		) -- restore last workspace session for current directory
+		-- Auto save session
+		vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+			callback = function()
+				for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+					-- Don't save while there's any 'nofile' buffer open.
+					if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "nofile" then
+						return
+					end
+				end
+				require("session_manager").save_current_session()
+			end,
+		})
+	end,
+}
