4da3cdafe87470c9722a41ac2a406658c58d4481
Øystein Sandvik <oysandvik94@gmail.com>
2024-07-28 12:23:44 +0200

    nvim: oil

--- dotfiles/.config/nvim/lua/langeoys/plugins/oil.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
 	"stevearc/oil.nvim",
 	config = function()
 		require("oil").setup({
-			default_file_explorer = false,
+			default_file_explorer = true,
 			columns = {
 				"icon",
 			},
