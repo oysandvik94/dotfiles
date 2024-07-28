430e3681dce9fd5b7f4a38580be38572b69f8174
Øystein Sandvik <oysandvik94@gmail.com>
2024-07-28 12:22:37 +0200

    nvim: fix curl bindings

--- dotfiles/.config/nvim/lua/langeoys/plugins/curl.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/curl.lua
@@ -1,6 +1,6 @@
 return {
-	-- dir = "~/dev/general/curl.nvim/",
-	"oysandvik94/curl.nvim",
+	dir = "~/dev/general/curl.nvim/",
+	-- "oysandvik94/curl.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
@@ -8,27 +8,27 @@ return {
 		local curl = require("curl")
 		curl.setup({})
 
-		vim.keymap.set("n", "<leader>cc", function()
+		vim.keymap.set("n", "<leader>hh", function()
 			curl.open_curl_tab()
 		end, { desc = "Open a curl tab scoped to the current working directory" })
 
-		vim.keymap.set("n", "<leader>co", function()
+		vim.keymap.set("n", "<leader>ho", function()
 			curl.open_global_tab()
 		end, { desc = "Open a curl tab with gloabl scope" })
 
-		vim.keymap.set("n", "<leader>csc", function()
+		vim.keymap.set("n", "<leader>hc", function()
 			curl.create_scoped_collection()
 		end, { desc = "Create or open a collection with a name from user input" })
 
-		vim.keymap.set("n", "<leader>cgc", function()
-			curl.create_global_collection()
-		end, { desc = "Create or open a global collection with a name from user input" })
-
-		vim.keymap.set("n", "<leader>fsc", function()
+		vim.keymap.set("n", "<leader>hf", function()
 			curl.pick_scoped_collection()
 		end, { desc = "Choose a scoped collection and open it" })
 
-		-- vim.keymap.set("n", "<leader>fgc", function()
+		-- vim.keymap.set("n", "<leader>cgc", function()
+		-- 	curl.create_global_collection()
+		-- end, { desc = "Create or open a global collection with a name from user input" })
+
+		-- vim.keymap.set("n", "<leader>fcg", function()
 		-- 	curl.pick_global_collection()
 		-- end, { desc = "Choose a global collection and open it" })
 	end,
