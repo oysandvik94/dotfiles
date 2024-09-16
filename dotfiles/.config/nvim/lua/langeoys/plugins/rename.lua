c1ab887986e15215f782abfd353d9f1db44a7f8f
Øystein Sandvik <oysandvik94@gmail.com>
2024-09-16 12:46:45 +0200

    nvim: rename plugin

--- dotfiles/.config/nvim/lua/langeoys/plugins/rename.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/rename.lua
@@ -1,9 +1,7 @@
 return {
 	"smjonas/inc-rename.nvim",
 	config = function()
-		require("inc_rename").setup({
-			input_buffer_type = "dressing",
-		})
+		require("inc_rename").setup({})
 		vim.cmd(":set inccommand=split")
 		vim.keymap.set("n", "<leader>lr", function()
 			return ":IncRename " .. vim.fn.expand("<cword>")
