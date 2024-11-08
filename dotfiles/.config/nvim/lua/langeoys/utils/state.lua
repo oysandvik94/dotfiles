721e356693732ade78f2d91f527c18a38005ea1c
Øystein Sandvik <oysandvik94@gmail.com>
2024-11-08 10:17:09 +0100

    state

--- dotfiles/.config/nvim/lua/langeoys/utils/state.lua
+++ dotfiles/.config/nvim/lua/langeoys/utils/state.lua
@@ -1,29 +1,37 @@
 local M = {}
 
-M.save_state = function(state_key, object)
-    P("saving state " .. state_key)
-    P(object)
+function get_state_file(state_key, cwd)
+	local state_file = vim.fn.stdpath("data") .. "/"
+	if cwd then
+		local workspace_path = vim.fn.getcwd()
+		local unique = vim.fn.fnamemodify(workspace_path, ":t") .. "_" .. vim.fn.sha256(workspace_path):sub(1, 8) ---@type string
+		state_file = state_file .. unique .. "_"
+	end
+
+	state_file = state_file .. state_key .. ".json"
+	return state_file
+end
+M.save_state = function(state_key, object, cwd)
 	local Path = require("plenary.path")
 
-	local state_file = vim.fn.stdpath("data") .. "/" .. state_key .. ".json"
+	local state_file = get_state_file(state_key, cwd)
 
 	Path:new(state_file):write(vim.fn.json_encode(object), "w")
 end
 
-M.get_state = function(state_key)
-    local Path = require("plenary.path")
+M.get_state = function(state_key, cwd)
+	local Path = require("plenary.path")
 
-    local state_file = vim.fn.stdpath("data") .. "/" .. state_key .. ".json"
+	local state_file = get_state_file(state_key, cwd)
 
-    if Path:new(state_file):exists() then
-        return vim.fn.json_decode(Path:new(state_file):read())
-    end
+	if Path:new(state_file):exists() then
+		return vim.fn.json_decode(Path:new(state_file):read())
+	end
 
-    return {}
+	return nil
 end
 
-M.MARK_STATE = "global_marks"
+M.FORMAT_STATE = "autoformat"
 M.COLOR_STATE = "color_scheme"
-
 
 return M
