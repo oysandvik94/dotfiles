158837aa58781c235fa7f9d0c01ef505b20a50c8
Øystein Sandvik <oysandvik94@gmail.com>
2024-11-08 10:13:33 +0100

    nvim: fix blink config

--- dotfiles/.config/nvim/lua/langeoys/plugins/lsp/blink.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/lsp/blink.lua
@@ -6,25 +6,17 @@ return {
 	version = "v0.*",
 	enabled = false,
 	opts = {
-		highlight = {
-			-- sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- useful for when your theme doesn't support blink.cmp
-			-- will be removed in a future release, assuming themes add support
-			use_nvim_cmp_as_default = true,
-		},
-		-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-		-- adjusts spacing to ensure icons are aligned
-		nerd_font_variant = "normal",
+		-- nerd_font_variant = "normal",
 
 		-- experimental auto-brackets support
 		accept = { auto_brackets = { enabled = true } },
 
 		-- experimental signature help support
 		trigger = { signature_help = { enabled = true } },
-		keymap = {
-			accept = { "<CR>" },
-			show_documentation = "<Tab>",
-		},
+		-- keymap = {
+		-- 	accept = { "<CR>" },
+		-- 	show_documentation = "<Tab>",
+		-- },
 		windows = {
 			documentation = {
 				min_width = 10,
