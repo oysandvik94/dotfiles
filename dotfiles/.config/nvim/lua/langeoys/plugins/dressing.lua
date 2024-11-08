e89fd60bf402243b852814ca75365d29a866431b
Øystein Sandvik <oysandvik94@gmail.com>
2024-11-08 10:13:25 +0100

    format

--- dotfiles/.config/nvim/lua/langeoys/plugins/dressing.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/dressing.lua
@@ -1,6 +1,6 @@
 return {
-    'stevearc/dressing.nvim',
-    config = function()
-        require("dressing").setup({})
-    end
+	"stevearc/dressing.nvim",
+	config = function()
+		require("dressing").setup({})
+	end,
 }
