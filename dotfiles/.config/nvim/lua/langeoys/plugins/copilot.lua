648118e2c7804dcae44d8edf3c50bde892c32ea2
Øystein Sandvik <oystein.sandvik@tietoevry.com>
2025-05-14 12:12:28 +0200

    nvim: enable completion in insert mode for copilot

--- dotfiles/.config/nvim/lua/langeoys/plugins/copilot.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/copilot.lua
@@ -3,7 +3,7 @@ return {
   init = function()
     vim.g.copilot_nes_debounce = 500
     vim.lsp.enable("copilot_ls")
-    vim.keymap.set("n", "<tab>", function()
+    vim.keymap.set({ "n", "i" }, "<tab>", function()
       -- Try to jump to the start of the suggestion edit.
       -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
       local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
