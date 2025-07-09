c961781c00d579537d8b6fe74dad433cec869efb
Øystein Sandvik <oystein.sandvik@tietoevry.com>
2025-07-09 15:25:42 +0200

    commit sway stuff that I dont care about

--- dotfiles/.config/sway/dropdown.sh
+++ dotfiles/.config/sway/dropdown.sh
@@ -1,14 +1,18 @@
-# Set your preferred terminal emulator
-set $title dropdown
-set $term kitty --title $title  
+#!/bin/bash
 
-# Execute your preferred terminal emulator in scratchpad  
-exec $term  
+# Get current output size
+read -r width height < <(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .current_mode | "\(.width) \(.height)"')
 
-# Move the terminal to scratchpad and set its size and position
-for_window [title="$title"] move container to scratchpad
-for_window [title="$title"] floating enable, resize set 800 600, move position center
+# Size as a percentage of screen
+term_width=$(awk "BEGIN { print int($width * 0.6) }")
+term_height=$(awk "BEGIN { print int($height * 0.4) }")
 
-# Bind your preferred shortcut to show / hide the terminal  
-bindsym $mod+t scratchpad show  
+# Position (centered)
+pos_x=$(awk "BEGIN { print int(($width - $term_width) / 2) }")
+pos_y=$(awk "BEGIN { print int(($height - $term_height) / 4) }") # top third
 
+# Launch terminal in floating mode, move and resize
+swaymsg exec "ghostty --title=dropterm"
+sleep 0.1
+swaymsg '[title="dropterm"] floating enable, resize set $term_width px $term_height px, move absolute position $pos_x px $pos_y px'
+swaymsg '[title="dropterm"] move to scratchpad, scratchpad show'
