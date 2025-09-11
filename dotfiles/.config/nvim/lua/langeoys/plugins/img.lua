5c14e1cd9e0f64b37e45ea1bb2bd0540363d8d37
Øystein Sandvik <oystein.sandvik@tietoevry.com>
2025-09-11 13:13:48 +0200

    make waybar cool

--- dotfiles/.config/nvim/lua/langeoys/plugins/img.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/img.lua
@@ -3,8 +3,13 @@ return {
     "HakonHarnes/img-clip.nvim",
     event = "VeryLazy",
     opts = {
-      -- add options here
-      -- or leave it empty to use the default settings
+      filetypes = {
+        codecompanion = {
+          prompt_for_file_name = false,
+          template = "[Image]($FILE_PATH)",
+          use_absolute_path = true,
+        },
+      },
     },
     keys = {
       -- suggested keymap
