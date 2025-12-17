09ffd8c1ef8f5bfd43eb3b41f1e1ee9d56aa4c33
Øystein Sandvik <oystein.sandvik@tietoevry.com>
2025-12-17 09:29:02 +0100

    i dont know

--- dotfiles/.config/nvim/lua/langeoys/plugins/mini.lua
+++ dotfiles/.config/nvim/lua/langeoys/plugins/mini.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "echasnovski/mini.ai",
-    version = false,
-    config = function()
-      require("mini.ai").setup()
-    end,
-  },
-  {
     "echasnovski/mini.test",
     version = false,
     config = function()
